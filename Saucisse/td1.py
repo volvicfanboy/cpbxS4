0efb1a92559e8539992ca79962b184ce6d669323
Z3ZEL <estebanrodriguez17780@gmail.com>
2022-02-07 08:15:16 +0100

    update

--- Saucisse/td1.py
+++ Saucisse/td1.py
@@ -1,45 +1,54 @@
 from tkinter import *
 
-myWindow=Tk()
+myWindow = Tk()
 # L=Label(myWindow,text = "blabla")
 # L.pack()
 # B=Button(myWindow,text = "Quitter",command = myWindow.destroy)
 # B.pack()
 # myWindow.mainloop()
 
-#Le label est un widget pour afficher du texte. Le button permet d'executer une method.
+# Le label est un widget pour afficher du texte. Le button permet d'executer une method.
 
 
-#Question 2.1
+# Question 2.1
 menu = Frame(myWindow)
 game = Frame(myWindow)
 menu.pack(side=LEFT)
 game.pack(side=RIGHT)
-label1 = Label(menu,text="Menu de configuration")
-label2 = Label(game,text="Plateau de jeu")
-plateau = Canvas(game,width=300,height=200)
+label1 = Label(menu, text="Menu de configuration")
+label2 = Label(game, text="Plateau de jeu")
+plateau = Canvas(game, width=300, height=200)
 plateau.pack()
 label1.pack()
 label2.pack()
 
 
-#Question 2.4
-def move(canvas,id,dx,dy):
-    (x1,y1,x2,y2)=canvas.coords(id)
-    (x3,y3,x4,y4) =  (x1+dx,y1+dy,x2+dx,y2+dy)
-    if x4>300 or y4>200 or x3<0 or y3 <0:
+# Question 2.4
+def move(canvas, id, dx, dy):
+    (x1, y1, x2, y2) = canvas.coords(id)
+    (x3, y3, x4, y4) = (x1 + dx, y1 + dy, x2 + dx, y2 + dy)
+    if x4 > 300 or y4 > 200 or x3 < 0 or y3 < 0:
         return
-    canvas.coords(id,(x3,y3,x4,y4)) 
+    canvas.coords(id, (x3, y3, x4, y4))
 
-#Pion
-pawn = plateau.create_oval(150,100,175,125)
 
+<<<<<<< Updated upstream
 #Quatre button de deplacement
 buttonR = Button(menu,text="Move Right",command=lambda : move(plateau,pawn,10,0))
 buttonL = Button(menu,text="Move Left",command=lambda :  move(plateau,pawn,-10,0))
 buttonU = Button(menu,text="Move Up",command=lambda : move(plateau,pawn,0,10))
 buttonD = Button(menu,text="Move Down",command=lambda : move(plateau,pawn,0,-10))
 buttonChangeColor = Button(menu,text="Change Color",command=lambda :changeColor(pawn))
+=======
+# Pion
+pawn = plateau.create_oval(150, 100, 175, 125)
+
+# Quatre button de deplacement
+buttonR = Button(menu, text="Move Right", command=lambda: move(plateau, pawn, 10, 0))
+buttonL = Button(menu, text="Move Left", command=lambda: move(plateau, pawn, -10, 0))
+buttonU = Button(menu, text="Move Up", command=lambda: move(plateau, pawn, 0, 10))
+buttonD = Button(menu, text="Move Down", command=lambda: move(plateau, pawn, 0, -10))
+>>>>>>> Stashed changes
 
 buttonL.pack()
 buttonD.pack()
@@ -47,6 +56,7 @@ buttonR.pack()
 buttonU.pack()
 buttonChangeColor.pack()
 
+<<<<<<< Updated upstream
 def changeToGreen(id):
     plateau.itemconfig(id,fill = "green")
 
@@ -58,13 +68,22 @@ def changeColor(id):
 
 
 #Question 3.1
-def keyIn(evt):
-    print("Vous avez appuy ́e sur la touche ",evt.char)
-myWindow.bind("<Key>",keyIn)
+=======
 
+def changeToGreen():
+    plateau.itemconfig(id, fill="green")
+
+
+# Question 3.1
+>>>>>>> Stashed changes
+def keyIn(evt):
+    print("Vous avez appuyé sur la touche ", evt.char)
+
+
+myWindow.bind("<Key>", keyIn)
 
 
 
 myWindow.mainloop()
 
-#emit from jetbrain
+# emit from jetbrain
